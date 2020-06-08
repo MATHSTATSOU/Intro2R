e8f38b53357b31482c1f1d3ee8632f1a10803693
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-06-08 18:46:53 -0500

    Completed data (ddt) with doc and added dplyr verbs and doc

--- R/firstfun.R
+++ R/firstfun.R
@@ -40,3 +40,25 @@ myfirstfun <- function(x, ...) { # key function "function(), name of function , 
   graphics::plot(x,y,main = "My first R function", ...) # plot function -- use ?plot to learn more about
   return(list(x=x,y=y)) # return will stop execution of code and release argument to the command line
 }
+
+
+
+
+#' @title DDT data set.
+#'
+#' @description  A dataset containing measurements on fish caught on the Tennessee River
+#'
+#' @details This is a standard data set from the course MATH 4753 taken from the data sets provided by the text book.
+#'
+#' @format A data frame with 144 rows and 6 variables:
+#' \describe{
+#'   \item{DDT}{ppm DDT in the flesh of fish}
+#'   \item{LENGTH}{Length of fish in cm}
+#'   \item{MILE}{Miles up river where fish is caught}
+#'   \item{RIVER}{River name}
+#'   \item{SPECIES}{Fish speccies}
+#'   \item{WEIGHT}{Weight of fish}
+#' }
+#'
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"ddt"
