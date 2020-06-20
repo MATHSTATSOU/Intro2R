76934ec31daf8f2ab4bddcd62ffa3089099a50db
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-06-20 16:08:43 -0500

    Added "fire" data

---
+++ R/fire.R
@@ -0,0 +1,14 @@
+#' @title Fire data set.
+#'
+#' @description  A data set concerning damage from fire
+#'
+#' @details This is a standard data set from the course MATH 4753 taken from the data sets provided by the text book.
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{Distance in miles from fire station}
+#'   \item{DAMAGE}{Thouands of dollars}
+#' }
+#'
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
