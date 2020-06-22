ca656685486be4fc215432fae80cb25970cdac1f
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-06-22 11:01:05 -0500

    New data

---
+++ R/accidents.R
@@ -0,0 +1,14 @@
+#' @title Accidents data set.
+#'
+#' @description  A data set concerning damage from accidents
+#'
+#' @details This is a standard data set from the course MATH 4753 taken from the data sets provided by the text book.
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{HOUR}{Hoursin miles from fire station}
+#'   \item{ACCIDENTS}{Number of dollars}
+#' }
+#'
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"accidents"
