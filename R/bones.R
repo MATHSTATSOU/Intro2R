49b25f7d00a8a6e36eb5cdb3a750e9a167d95cd6
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-06-23 22:15:47 -0500

    New vignetet for bones data set

---
+++ R/bones.R
@@ -0,0 +1,13 @@
+#' @title Bones data set.
+#'
+#' @description  A data set used in determining using Length to Width ratios
+#'
+#' @details This is a standard data set from the course MATH 4753 taken from the data sets provided by the text book.
+#'
+#' @format A data frame with 41 rows and 1 variable:
+#' \describe{
+#'   \item{LWRATIO}{Length to Width ratio for Humerus bones}
+#' }
+#'
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"bones"
