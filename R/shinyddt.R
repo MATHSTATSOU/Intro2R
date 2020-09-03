298849d8a8dc3c6f9ee8ebec1f7bdf26c82c3cd1
Wayne Stewart <wayne.s.stewart@ou.edu>
2020-09-03 13:35:40 -0500

    Shiny adjustments

---
+++ R/shinyddt.R
@@ -0,0 +1,39 @@
+#' @title Dynamic plots using shiny with ddt
+#'
+#'
+#'
+#' @description Widgets, plotting areas and layouts should be mastered
+#'
+#' @details  Please note the basic structure of the server input output
+#' @return  Plots
+#'
+#' @section shiny:
+#' The shiny web server will run once this function is invoked and will open a web browser. You should learn how this is implemented.
+#'
+#' The web server can be studied \url{https://shiny.rstudio.com/tutorial/}
+#'
+#' @section Shiny server:
+#' When building the server function in the app make sure your remember the three steps:
+#'
+#' \enumerate{
+#' \item Must populate the output list
+#' \item Must use Render functions
+#' \item Must use the input list
+#' }
+#'
+#' @section Shiny reactivity:
+#'
+#' The main engine that drives \emph{shiny server} is the concept of \emph{reactivity} this is what makes shiny apps responsive.
+#' The concept is a very important one and you must understand it completely by visiting a video and then reading an article.
+#' \enumerate{
+#' \item Please visit \href{https://vimeo.com/rstudioinc/review/131218530/212d8a5a7a/#t=47m27s}{Reactivity}
+#' \item Read \href{https://shiny.rstudio.com/articles/reactivity-overview.html}{this informative article}
+#' }
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{ shinyddt()}
+shinyddt<-function(){
+  shiny::runApp(system.file("shinygg/investddt", package="Intro2R"),launch.browser = TRUE)
+}
---
+++ inst/shinygg/investddt/app.R
@@ -0,0 +1,84 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+myout <- function(x) ifelse(abs(x) > 3, "OUTLIER",
+                            ifelse(abs(x)>=2 & abs(x)<=3, "Poss. Out.", "MAIN"))
+
+ddt<-ddt%>%mutate(zL = scale(LENGTH), zW = scale(WEIGHT), zM = scale(MILE), zD = scale(DDT), zLo = myout(zL), zWo = myout(zW),zMo = myout(zM), zDo = myout(zD) )
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("DDT data set"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            varSelectInput("condvar",
+                           "Categorical fill Variable:",
+                           ddt[,c("SPECIES","RIVER")],
+                           selected =  "SPECIES"),
+            varSelectInput("var",
+                           "Quantitative Variable:",
+                           ddt[,c("MILE","LENGTH", "WEIGHT","DDT")],
+                          selected =  "LENGTH"),
+            varSelectInput("Zout",
+                           "Extreme Quant. values?:",
+                           ddt[,c("zLo","zWo", "zDo")],
+                           selected =  "zLo"),
+
+        ),
+
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("histPlot"),
+           plotOutput("dotPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$histPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+      g <- ggplot(ddt, aes(x = !!input$var, fill = !!input$condvar ))
+      g <- g + geom_histogram(bins = input$bins)
+
+
+        # draw the histogram with the specified number of bins
+        g
+    })
+    output$dotPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+
+        g <- ggplot(ddt, aes(x = LENGTH))
+        g <- g + geom_dotplot(aes(fill = zLo))
+        g <- g + geom_density(aes(y = ..count..))
+
+
+        # draw the histogram with the specified number of bins
+        g
+    })
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
