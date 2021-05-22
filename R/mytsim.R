28351ff910b4ed74ccb6857890cb2e02c6bee6fd
Wayne Stewart <wayne.s.stewart@ou.edu>
2021-05-22 10:04:45 -0500

    Change defaults and bar colors

--- R/mytsim.R
+++ R/mytsim.R
@@ -20,7 +20,7 @@
 #'
 #' @examples
 #' myTsim()
-myTsim<-function(n1=10,sigma1=3,mean1=5,iter=1000,ymax=0.4,x=2,y=0.3,...){    # adjust ymax to make graph fit
+myTsim<-function(n1=20,sigma1=3,mean1=5,iter=10000,ymax=0.4,x=2,y=0.3,...){    # adjust ymax to make graph fit
   y1=rnorm(n1*iter,mean=mean1,sd=sigma1)# generate iter samples of size n1
 
   data1.mat=matrix(y1,nrow=n1,ncol=iter,byrow=TRUE) # Each column is a sample size n1
@@ -30,12 +30,17 @@ myTsim<-function(n1=10,sigma1=3,mean1=5,iter=1000,ymax=0.4,x=2,y=0.3,...){    # 
 
   w=(ybar-mean1)/(sd1/sqrt(n1))      #T stat
 
+  v <- hist(w,plot = FALSE)
+  nbar <- length(v$mids)
+  relf <- v$density/max(v$density)
+  colbar <- rgb(relf^2,1-relf^3, relf^4,0.3)
+
   hist(w,freq=FALSE, ylim=c(0,ymax), # Histogram with annotation
-       main=substitute(paste("Sample size = ",n[1]," = ",n1," statistic = ",T," iterations= ",iter)),
+       main=substitute(paste("Sample size = ",n[1]," = ",n1," statistic = ",T," iterations= ",iter)), col = colbar,
        xlab=expression(paste(T, "Statistic",sep=" ")), las=1,...)
   lines(density(w),col="Blue",lwd=3) # add a density plot
   curve(dt(x,n1-1),add=TRUE,col="Red",lty=2,lwd=3) # add a theoretical curve
-  title=expression(T==frac((bar(y)-mu),s/sqrt(n1))) #mathematical annotation -see ?plotmath
+  title=expression(T==frac((bar(y)-mu),s/sqrt(n[1]))) #mathematical annotation -see ?plotmath
   legend(x,y,c("Simulated","Theoretical"),col=c("Blue","Red"),lwd=4,lty=1:2,bty="n",title=title) # Legend #
   invisible(list(w=w,summary=summary(w),sd=sd(w),fun="T")) # some output to use if needed
 }
