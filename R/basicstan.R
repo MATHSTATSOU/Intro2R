d21d015465828cc79a5d19e958c78dea883a708d
Wayne Stewart <wayne.s.stewart@ou.edu>
2022-01-03 23:00:47 -0600

    Adding BAYESIAN analyses using STAN. First function estimating mu from a sample (ddt).

---
+++ R/basicstan.R
@@ -0,0 +1,43 @@
+#' @title Run a Bayesian model using STAN
+#'
+#'
+#'
+#' @description A basic Bayesian model estimating a population mean
+#'
+#' @details  Please note the basic structure of the server input output
+#' @return  An MCMC object
+#'
+#' @section STAN:
+#' This function will start a STAN instance creating a STAN object which can be viewed using other packages.
+#'
+#'
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{ basicstan()}
+basicstan<-function(){
+ddt <- Intro2R::ddt
+basic_data <- list(y=ddt$LENGTH, N=length(ddt$LENGTH))
+
+fit <-rstan::stan(paste0(system.file("Bayes", package="Intro2R"),"/basic.stan"),
+              model_name = "basic",
+              data = basic_data,
+              chains = 3,
+              warmup = 1000,
+              cores = 3,
+              iter = 5000,
+              pars = c("mu")
+  )
+
+
+
+  mu <- rstan::extract(fit)[[1]]
+
+  g <- ggplot2::qplot(mu,main = expression(mu))
+
+  print(g)
+
+  print(fit,probs=c(0.025,0.5,0.975))
+
+}
